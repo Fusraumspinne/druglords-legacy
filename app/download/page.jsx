554ad126d871d6f9c06ca50af8618eb128958776
Fusraumspinne <mjjaeger2002@gmail.com>
2024-09-21 20:51:29 +0200

    initial upload

---
+++ app/download/page.jsx
@@ -0,0 +1,70 @@
+"use client"
+
+import React from 'react'
+import Navbar from '@/components/navbar'
+import { Button, Offcanvas } from 'react-bootstrap';
+import DownloadIcon from '@mui/icons-material/Download';
+import { useState, useEffect } from 'react';
+import Image from 'next/image'
+import InfoIcon from '@mui/icons-material/Info';
+
+function page() {
+    const [releaseDate, setReleaseDate] = useState("2024-9-10")
+    const [isDownloadEnabled, setIsDownloadEnabled] = useState(false);
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    useEffect(() => {
+        const today = new Date();
+        const release = new Date(releaseDate);
+
+        if (release > today) {
+            setIsDownloadEnabled(false);
+        } else {
+            setIsDownloadEnabled(true);
+        }
+    }, [releaseDate]);
+
+    return (
+        <div>
+            <Navbar />
+
+            <Offcanvas show={show} onHide={handleClose} className='bg-dark text-success'>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title >Offcanvas</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                    Some text as placeholder. In real life you can have the elements you
+                    have chosen. Like, text, images, lists, etc.
+                </Offcanvas.Body>
+            </Offcanvas>
+
+            <div style={{ marginTop: "100px" }} className='d-flex justify-content-center'>
+                <div className='custom-card outline-success p-4'>
+                    <div className='d-flex p-0 m-0'>
+                        <p className='fs-1 text-success m-0 p-0'>Game Download</p>
+                        <Button className='bg-dark text-success m-0 p-0 ms-2' style={{border: "none"}} onClick={handleShow}><InfoIcon/></Button>
+                    </div>
+                    <p className='text-success'>Realese Date: {releaseDate}</p>
+
+                    <Image className='custom-img mb-4' src={"/img/Platzhalter.png"} width={350} height={350} />
+
+                    {isDownloadEnabled ? (
+                        <Button className='bg-dark button-100' variant="outline-success">
+                            <a className='text-success text-decoration-none' href="/Druglord's Legacy.zip" download><DownloadIcon /> Download</a>
+                        </Button>
+                    ) : (
+                        <Button className='bg-dark button-100' variant="outline-success">
+                            <a className='text-success text-decoration-none' href="/download"><DownloadIcon /> Download</a>
+                        </Button>
+                    )}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default page
---
+++ components/navbar.jsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { Button, Container, Form, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+
+function navbar() {
+    return (
+        <Navbar expand="lg" className="bnavbar-dark bg-dark">
+            <Container fluid>
+                <Navbar.Brand href="#" className='text-success'>Druglord's Legacy</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        <Nav.Link href="/download" className='text-success'>Game Download</Nav.Link>
+                        <Nav.Link href="" className='text-success'>Roadmap</Nav.Link>#
+                        <Nav.Link href="" className='text-success'>Communityhub</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default navbar
