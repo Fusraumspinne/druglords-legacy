095609280d38fc3ecf1e6bafab09d912edf28836
Fusraumspinne <mjjaeger2002@gmail.com>
2024-09-22 10:35:25 +0200

    bug fix und bild

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,8 @@ export default function RootLayout({ children }) {
       <Head>
         <meta name="description" content={metadata.description} />
         <meta property="og:title" content="Druglord's Legacy - Drug Tycoon Game" />
-        <meta property="og:description" content="Experience the thrill of cannabis cultivation and trade. Manage your resources and grow your empire in Druglord's Legacy!" />
-        <meta property="og:image" content="/img/Platzhalter.png" />
+        <meta property="og:description" content="Experience the thrill of drug cultivation and trade. Manage your resources and grow your empire in Druglord's Legacy!" />
+        <meta property="og:image" content="https://druglords-legacy.vercel.app/_next/image?url=%2Fimg%2FPlatzhalter.png&w=384&q=75" />
         <meta property="og:url" content="https://druglords-legacy.vercel.app" />
         <meta property="og:type" content="website" />
         <meta name="robots" content="index, follow" />
