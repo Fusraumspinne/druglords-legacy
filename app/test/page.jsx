f6de0edc2e5a6c136d5e3518f06d7a22477510ed
Fusraumspinne <mjjaeger2002@gmail.com>
2024-10-22 10:37:39 +0200

    test  file rust clone

---
+++ app/test/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Page() {
+    return (
+        <div>
+            <a href="/RustCloneGame.zip" download>
+                Download
+            </a>
+        </div>
+    )
+}
+
+export default Page
