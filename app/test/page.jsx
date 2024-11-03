e39ed2b265bb9ad82b94fcd7f4d25716d100d2ee
Fusraumspinne <mjjaeger2002@gmail.com>
2024-11-03 17:19:51 +0100

    test von game

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Page() {
     return (
         <div>
-            <a href="/RustCloneGame.zip" download>
+            <a href="/Cooking Disaster.zip" download>
                 Dont Download
             </a>
         </div>
