65a91964a94b8c57129bd1dd05e7536e8ea5544f
Fusraumspinne <mjjaeger2002@gmail.com>
2025-01-25 13:37:10 +0100

    bug fix

--- app/download/page.jsx
+++ app/download/page.jsx
@@ -103,8 +103,7 @@ function Page() {
                             <p>Windows Version | Download Counter: {downloadCounter}</p>
 
                             <Image className="w_100 h_100 border_o" src={"/img/Hauptmenu.png"} width={1920} height={1080} />
-
-                            <a className="text-decoration-none text_white" href="https://dl.dropboxusercontent.com/scl/fi/esx30gix14eyo5ez2lu5x/Druglord-s-Legacy.zip?rlkey=7nola1yk8o238ilyct1stdc7p&st=1301b17l">
+                            <a className="text-decoration-none text_white" href="https://dl.dropboxusercontent.com/scl/fi/s1f22vdid0nbmvoyf3tp3/Druglord-s-Legacy.zip?rlkey=sawvsyxy5opbt61qt5kvwh02s&st=2kwkt6oz">
                                 <Button className="custom_btn border-0 w_100 border_u" onClick={handleCounterUpdate}>
                                     <div className="normal_font"><DownloadIcon /> Download - Beta Version v.0.7.1 | Size ~ 750MB</div>
                                 </Button>
